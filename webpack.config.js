ba9246bd2010e473c0d45c3a00ff0e44525f8eae
mkum73 <manish32.nith@gmail.com>
2017-04-01 13:33:09 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+switch (process.env.NODE_ENV) {
+  case 'prod':
+    module.exports = require('./config/webpack.prod')({env: 'prod'});
+    break;
+  case 'test':
+    module.exports = require('./config/webpack.test')({env: 'test'});
+    break;
+  case 'dev':
+  default:
+    module.exports = require('./config/webpack.dev')({env: 'dev'});
+}
